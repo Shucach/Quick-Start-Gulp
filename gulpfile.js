9a26ef75d01a5b7782b734c9eea3eacb2851dee4
Sparrow <i@altsolution.net>
2019-07-17 21:37:03 +0300

    Convert svg to fonts

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ const rename = require('gulp-rename');
 const browserSync = require("browser-sync").create();
 const svgSprite = require('gulp-svg-sprite');
 
+const iconfont = require('gulp-iconfont');
+const iconfontCss = require('gulp-iconfont-css');
+
 const webp = require('gulp-webp');
 
 
@@ -22,6 +25,7 @@ function style() {
     return gulp.src([
         'app/css/reset.styl',
         'app/css/grid.styl',
+        'app/css/fonts.styl',
         'app/css/style.styl'
     ])
         .pipe(concat('main.styl'))
@@ -152,6 +156,24 @@ function browserSyncWatch() {
     gulp.watch("./*.html").on('change', browserSync.reload);
 }
 
+/**
+ * Convert svg icons to fonts
+ */
+function svg2Fonts() {
+    return gulp.src(['app/svg2Fonts/*.svg'])
+        .pipe(iconfontCss({
+            fontName: 'svgFonts',
+            targetPath: '_icons.scss',
+            fontPath: 'fonts/svg-fonts/'
+        }))
+        .pipe(iconfont({
+            fontName: 'svgFonts',
+            //prependUnicode: true,
+            formats: ['ttf', 'eot', 'woff', 'svg', 'woff2'],
+        }))
+        .pipe(gulp.dest('fonts/svg-fonts/'));
+}
+
 
 /**
  * Define complex tasks
@@ -163,6 +185,7 @@ const svg_sprite = gulp.series(createSvgSprite);
 const webP = gulp.series(imageToWebP);
 const watch = gulp.parallel(watchFiles);
 const serve = gulp.parallel(browserSyncWatch);
+const svgFonts = gulp.parallel(svg2Fonts);
 
 
 /**
@@ -176,3 +199,4 @@ exports.watch = watch;
 exports.serve = serve;
 exports.svg_sprite = svg_sprite;
 exports.webp = webP;
+exports.svg2Fonts = svgFonts;
