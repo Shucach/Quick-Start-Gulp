2647c5e1bba9288d829f1600f8ecddb4b3e520e0
Ivan Horobets <vanzzosolutions@gmail.com>
2020-09-13 19:36:15 +0300

    Change updater

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
-git remote add origin git@github.com:Shucach/Quick-Start-Gulp.git
-git pull origin master
+#After init git set remote url
+#git remote add origin git@github.com:Shucach/Quick-Start-Gulp.git
+
+git fetch origin
+git reset --hard origin/master
+
 echo "Done!"
